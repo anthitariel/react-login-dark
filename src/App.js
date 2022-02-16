2fabb732c0dbf42c02bfefbf941f9c670ff53d43
Anfisa <anfiya17@gmail.com>
2022-02-16 09:43:07 +0200

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { CssBaseline } from "@mui/material";
+//import SignIn from "./pages/SignIn";
+import CreateAccount from "./pages/CreateAccount";
 
-function App() {
+const theme = createTheme({
+  palette: {
+    mode: "dark",
+    primary: {
+      main: "#1976d2",
+      light: "#42a5f5",
+      dark: "#1565c0",
+      contrastText: "#fff",
+    },
+    background: {
+      default: "#2A2939",
+      paper: "#3D3E4B",
+    },
+  },
+});
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <CreateAccount />
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
---
+++ src/pages/SignIn.js
@@ -0,0 +1,142 @@
+import * as React from "react";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Dialog from "@mui/material/Dialog";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import FormGroup from "@mui/material/FormGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
+import InputAdornment from "@mui/material/InputAdornment";
+import Box from "@mui/material/Box";
+import InputLabel from "@mui/material/InputLabel";
+import { AppBar, Toolbar } from "@mui/material";
+import logo from "../images/logo.png";
+
+export default function SignIn() {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <Box
+      component="form"
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        background: "linear-gradient(to left top, #252436, #363744, #464553)",
+        minHeight: "100vh",
+      }}
+    >
+      <Button
+        variant="contained"
+        onClick={handleClickOpen}
+        size="medium"
+        sx={{
+          fontSize: 20,
+          textTransform: "capitalize",
+        }}
+      >
+        Sign In (Page 1)
+      </Button>
+      <AppBar sx={{ background: "#2A2939" }}>
+        <Toolbar>
+          <Link href="#" underline="none">
+            <img src={logo} alt="logo" />
+          </Link>
+        </Toolbar>
+      </AppBar>
+
+      <Dialog fullWidth="lg" maxWidth="sm" open={open} onClose={handleClose}>
+        <DialogContent sx={{ p: 10 }}>
+          <DialogTitle sx={{ fontSize: "h3.fontSize", p: 0 }}>
+            Sign In
+          </DialogTitle>
+          <DialogContentText sx={{ mb: 5, fontSize: "h5.fontSize" }}>
+            Login to manage your account
+          </DialogContentText>
+          <InputLabel
+            sx={{
+              fontSize: "h6.fontSize",
+              fontWeight: "bold",
+            }}
+          >
+            Username
+          </InputLabel>
+          <TextField
+            margin="normal"
+            id="username"
+            label="Username"
+            type="username"
+            variant="outlined"
+            fullWidth
+          />
+          <InputLabel
+            sx={{
+              mt: 2,
+              fontSize: "h6.fontSize",
+              fontWeight: "bold",
+            }}
+          >
+            Password
+          </InputLabel>
+          <TextField
+            margin="normal"
+            id="password"
+            label="Password"
+            type="password"
+            fullWidth
+            variant="outlined"
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="start">
+                  <VisibilityOffIcon />
+                </InputAdornment>
+              ),
+            }}
+          />
+          <FormGroup sx={{ mb: 2 }}>
+            <FormControlLabel control={<Checkbox />} label="Remember me" />
+          </FormGroup>
+          <Button
+            variant="contained"
+            size="medium"
+            fullWidth
+            onClick={handleClose}
+            sx={{
+              mt: 2,
+              fontSize: 20,
+              p: 1.5,
+              textTransform: "capitalize",
+            }}
+          >
+            Sign In
+          </Button>
+          <DialogContentText sx={{ mt: 3, textAlign: "center" }}>
+            <Link
+              href="#"
+              underline="none"
+              sx={{
+                color: "#5790FF",
+                fontSize: "h6.fontSize",
+                fontWeight: "bold",
+              }}
+            >
+              Forgot password?
+            </Link>
+          </DialogContentText>
+        </DialogContent>
+      </Dialog>
+    </Box>
+  );
+}
